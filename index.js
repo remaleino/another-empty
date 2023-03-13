64813cb2c304e6b9515bcdbb1d0a184184876ca2
remaleino <raimond.leino@gmail.com>
2023-03-13 21:55:12 +0200

    update 1.1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const axios = require('axios');
 
 try {
+    /*
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
@@ -9,7 +10,15 @@ try {
     core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    console.log(`The event payload: ${payload}`);*/
+    axios({
+        method: 'get',
+        url: core.getInput('reg-address')
+    }).then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+        console.log(error);
+    })
 } catch (error) {
     core.setFailed(error.message);
 }
